0db6284018fea80f7969fffa39a58bf4878b385d
Chukwuemeka Ngumoha <85541535+PC-Ngumoha@users.noreply.github.com>
2023-03-15 07:54:08 +0000

    Added dynamic data

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Lagos, Lagos State',
+    'salary': 'NGN. 1,000,000.00'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Abuja, F.C.T',
+    'salary': 'NGN. 500,000.00'
+  },
+  {
+    'id': 3,
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+    'salary': 'USD. 10,000.00'
+  },
+  {
+    'id': 4,
+    'title': 'Frontend Engineer',
+    'location': 'San Francisco, California',
+    'salary': 'USD. 12,000.00'
+  }
+]
 
 
 @app.route('/')
 def hello_world():
   """Displays 'Hello World!' on the browser.'"""
-  return '<h1>Hello World!</h1>'
+  return render_template('home.html', jobs=JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
